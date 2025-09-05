2869a54eefa4fd4d49bb0757f53749b693887fe4
Adi Jain <jainadi1717@gmail.com>
2025-09-05 12:59:01 +0530

    add: viewAllResult page added

---
+++ src/pages/admin/ViewAllResultAdmin.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ViewAllResultsAdmin() {
+    return (
+        <div sx={{color: 'black'}}>View All result component</div>
+    )
+}
+
+export default ViewAllResultsAdmin
+
